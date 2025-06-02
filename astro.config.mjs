e6718825c32738d5b4804655b4bac9def9022bac
Mahad Kalam <mahadkalam1@proton.me>
2025-06-02 20:02:05 +0100

    feat: design wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "**.amazonaws.com",
+          },
+      ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/content.config.ts
@@ -0,0 +1,38 @@
+import { z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { notionLoader } from "notion-astro-loader";
+import {
+  notionPageSchema,
+  transformedPropertySchema,
+} from "notion-astro-loader/schemas";
+
+const posts = defineCollection({
+  loader: notionLoader({
+    auth: import.meta.env.NOTION_TOKEN,
+    database_id: import.meta.env.NOTION_DATABASE_ID,
+    filter: {
+      property: "Published",
+      checkbox: { equals: true },
+    },
+    sorts: [{ property: "Date", direction: "descending" }],
+  }),
+  schema: notionPageSchema({
+    properties: z.object({
+      Name: transformedPropertySchema.title,
+      Description: transformedPropertySchema.rich_text,
+      Date: transformedPropertySchema.date.transform((property) => {
+        return property?.start;
+      }),
+      Published: transformedPropertySchema.checkbox,
+      Slug: transformedPropertySchema.rich_text.refine(
+        (slug) => /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(slug),
+        {
+          message: "Slug must be lowercase, alphanumeric, and kebab-case",
+        }
+      ),
+      //   Authors: transformedPropertySchema.people.optional(),
+    }),
+  }),
+});
+
+export const collections = { posts };
